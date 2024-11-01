281b43a0d611faf329a9d24c8dae7fc7d0cb9302
Mustafa <mustafashn.1@outlook.com>
2024-11-01 21:22:52 +0300

    Add project files.

---
+++ Sayı bulma Uygulaması Console ile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayı_bulma_Uygulaması_Console_ile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
