c3f6abe643b03b00d79fe2aaf852343493714fe3
Mustafa <mustafashn.1@outlook.com>
2024-11-01 21:51:16 +0300

    Veri tipleri, şart blokları, metodlar, döngüleri kapsayan bir öğrenim Sayı bulma uygulaması.

--- Sayı bulma Uygulaması Console ile/Program.cs
+++ Sayı bulma Uygulaması Console ile/Program.cs
@@ -10,6 +10,38 @@ namespace Sayı_bulma_Uygulaması_Console_ile
     {
         static void Main(string[] args)
         {
+
+            SayibulmaUygulaması();
+        }
+
+
+
+        static void SayibulmaUygulaması()
+        {
+            Random random = new Random();
+            int hedefSayisi = random.Next(1, 101);
+            int tahmin;
+            int tahminSayisi = 0;
+
+            Console.WriteLine("1 ila 100 arasında bir sayı giriniz.");
+
+            do
+            {
+                Console.WriteLine("Tahmininiz :");
+                tahmin = int.Parse(Console.ReadLine());
+                tahminSayisi++;
+
+                if (tahmin < hedefSayisi)
+                    Console.WriteLine("Daha büyük bir sayı giriniz");
+                else if (tahmin > hedefSayisi)
+                    Console.WriteLine("Daha küçük bir sayı giriniz");
+                else
+                    Console.WriteLine($"*******************  Tebrikler { tahminSayisi} denemede bildiniz  ******************* ");
+
+            } while (tahmin != hedefSayisi);
+
+            Console.WriteLine($"*******************  Tebrikler Doğru Sayi { tahmin}        *******************");
+            Console.ReadLine(); 
         }
     }
 }
